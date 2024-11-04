b61d59d60de0cb5d182f9d38973239c5e6fbcad4
Vasil Topalovic <vasiltopalovic@outlook.com>
2024-11-04 18:20:05 -0500

    feat: fix routes

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,27 +1,25 @@
 package seg3x02.employeeGql.resolvers
 
 import org.springframework.stereotype.Controller
-import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
-import org.springframework.data.mongodb.core.query.Query
-import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import seg3x02.employeeGql.entity.Employee
 import seg3x02.employeeGql.repository.EmployeesRepository
 import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.UUID
 
 @Controller
-class EmployeesResolver(val mongoOperations: MongoOperations, private val employeeRepository: EmployeesRepository) {
-	@MutationMapping
-	fun employees(@Argument id: Int): List<Employee> {
-		val query = Query()
-		query.addCriteria(Criteria.where("id").`is`(id))
-		return mongoOperations.find(query, Employee::class.java)
+class EmployeesResolver(private val employeeRepository: EmployeesRepository) {
+	@QueryMapping
+	fun employees(): List<Employee> {
+		return employeeRepository.findAll()
 	}
 
 	@MutationMapping
 	fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput) : Employee {
 		val employee = Employee(input.name ?: "name", input.dateOfBirth ?: "dob", input.city ?: "city", input.salary ?: 0.0f, input.gender, input.email)
+		employee.id = UUID.randomUUID().toString()
 		employeeRepository.save(employee)
 		return employee
 	}
