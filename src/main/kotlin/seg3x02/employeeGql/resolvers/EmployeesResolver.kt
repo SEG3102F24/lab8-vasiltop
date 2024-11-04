ac86f019d9eff775af47c0517c42d518a10c439f
vasiltop <vasiltopalovic@outlook.com>
2024-11-04 16:57:13 -0500

    feat: add routes

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,28 @@
 package seg3x02.employeeGql.resolvers
 
 import org.springframework.stereotype.Controller
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Criteria
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(val mongoOperations: MongoOperations, private val employeeRepository: EmployeesRepository) {
+	@MutationMapping
+	fun employees(@Argument id: Int): List<Employee> {
+		val query = Query()
+		query.addCriteria(Criteria.where("id").`is`(id))
+		return mongoOperations.find(query, Employee::class.java)
+	}
 
+	@MutationMapping
+	fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput) : Employee {
+		val employee = Employee(input.name ?: "name", input.dateOfBirth ?: "dob", input.city ?: "city", input.salary ?: 0.0f, input.gender, input.email)
+		employeeRepository.save(employee)
+		return employee
+	}
 }
